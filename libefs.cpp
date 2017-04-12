bab92e227b301c7eac1b4d8708ec9eef3b484b23
cxingkai <cxingkai@yahoo.com.sg>
2017-04-12 22:58:32 +0800

    Edit

--- libefs.cpp
+++ libefs.cpp
@@ -3,11 +3,14 @@
 // FS Descriptor
 TFileSystemStruct *_fs;
 
+// Array of file pointers
+FILE* files[1000];
+
 // Open File Table
 TOpenFile *_oft;
 
 // File pointer, because fuck structs
-FILE *_fp;
+FILE *_filePtr;
 
 // Open file table counter
 int _oftCount=0;
@@ -18,6 +21,7 @@ void initFS(const char *fsPartitionName, const char *fsPassword)
 {
 	mountFS(fsPartitionName, fsPassword);
 	_fs = getFSInfo();
+	_oft = new TOpenFile();
 }
 
 // Opens a file in the partition. Depending on mode, a new file may be created
@@ -28,8 +32,8 @@ void initFS(const char *fsPartitionName, const char *fsPassword)
 int openFile(const char *filename, unsigned char mode)
 {	
 	//_oft = (TOpenFile *)malloc(sizeof(TOpenFile));
-	_oft = new TOpenFile();
 	_oft->openMode = mode;
+	//printf("%d\n", _oft->openMode);
 	_oft->blockSize = _fs->blockSize;
 	_oft->inodeBuffer = makeInodeBuffer();
 	_oft->buffer = makeDataBuffer();
@@ -52,8 +56,8 @@ int openFile(const char *filename, unsigned char mode)
 	{
 		case MODE_NORMAL:
 			if(fileNdx == FS_FILE_NOT_FOUND){
-				printf("Cannot find encrypted file %s\n", filename);
-				exit(-1);
+				//printf("Cannot find encrypted file %s\n", filename);
+				return -1;
 			}
 			_oft->inode = getInodeForFile(filename);
 			loadInode(_oft->inodeBuffer, _oft->inode);
@@ -61,7 +65,7 @@ int openFile(const char *filename, unsigned char mode)
 			
 			// calculate byte offset
 			byteIndex = _fs->dataByteIndex + (blockNum-1) * _fs->blockSize;
-			fseek(_fp, byteIndex, SEEK_SET);
+			fseek(_filePtr, byteIndex, SEEK_SET);
 			
 			//_oft->filePtr = *_fp;
 
@@ -78,6 +82,7 @@ int openFile(const char *filename, unsigned char mode)
 				
 				// Mark the free block now as busy
 				markBlockBusy(freeBlock);
+				return 1;
 				
 			}
 			_oft->inode = getInodeForFile(filename);
@@ -85,14 +90,14 @@ int openFile(const char *filename, unsigned char mode)
 			blockNum = _oft->inodeBuffer[0];
 			
 			byteIndex = _fs->dataByteIndex + (blockNum-1) * _fs->blockSize;
-			fseek(_fp, byteIndex, SEEK_SET);
+			fseek(_filePtr, byteIndex, SEEK_SET);
 			
 			//_oft->filePtr = _fp;
 			break;
 		case MODE_READ_ONLY:
 			if(fileNdx == FS_FILE_NOT_FOUND){
-				printf("Cannot find encrypted file %s\n", filename);
-				exit(-1);
+				//printf("Cannot find encrypted file %s\n", filename);
+				return -1;
 			}
 			_oft->inode = getInodeForFile(filename);
 			loadInode(_oft->inodeBuffer, _oft->inode);
@@ -100,14 +105,14 @@ int openFile(const char *filename, unsigned char mode)
 			printf("%lu", blockNum);
 			
 			byteIndex = _fs->dataByteIndex + (blockNum-1) * _fs->blockSize;
-			fseek(_fp, byteIndex, SEEK_SET);
+			fseek(_filePtr, byteIndex, SEEK_SET);
 			
 			//_oft->filePtr = _fp;
 			break;
 		case MODE_READ_APPEND:
 			if(fileNdx == FS_FILE_NOT_FOUND){
-				printf("Cannot find encrypted file %s\n", filename);
-				exit(-1);
+				//printf("Cannot find encrypted file %s\n", filename);
+				return -1;
 			}
 			_oft->inode = getInodeForFile(filename);
 			loadInode(_oft->inodeBuffer, _oft->inode);
@@ -115,19 +120,29 @@ int openFile(const char *filename, unsigned char mode)
 			
 			byteIndex = _fs->dataByteIndex + (blockNum-1) * _fs->blockSize;
 			//filePtr starts from back of file
-			fseek(_fp, byteIndex, SEEK_END);
+			fseek(_filePtr, byteIndex, SEEK_END);
 			
 			//_oft->filePtr = _fp;
 			break;
 		default:
-			return -1;
+			return -2;
 	}
+	return 0;
 }
 
 // Write data to the file. File must be opened in MODE_NORMAL or MODE_CREATE modes. Does nothing
 // if file is opened in MODE_READ_ONLY mode.
 void writeFile(int fp, void *buffer, unsigned int dataSize, unsigned int dataCount)
 {
+	char mode = _oft->openMode;
+	if (mode == MODE_NORMAL || mode == MODE_CREATE)
+	{
+		
+	}
+	else if (mode == MODE_READ_APPEND)
+	{
+		
+	}
 }
 
 // Flush the file data to the disk. Writes all data buffers, updates directory,
@@ -139,18 +154,11 @@ void flushFile(int fp)
 // Read data from the file.
 void readFile(int fp, void *buffer, unsigned int dataSize, unsigned int dataCount)
 {
-	/*
-	readBlock(_oft->buffer, blockNum);
+	
+	//readBlock(_oft->buffer, blockNum);
 
-	unmountFS();
-
-	fwrite(_oft->buffer, sizeof(char), len, fp);
-
-	fclose(fp);
-
-	free(_oft->inodeBuffer);
-	free(_oft->buffer);
-	*/
+	//fwrite(_oft->buffer, sizeof(char), dataSize, fp);
+	
 }
 
 // Delete the file. Read-only flag (bit 2 of the attr field) in directory listing must not be set. 
